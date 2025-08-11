e5c69398c8b1a0089fff7dd8c47a9c13fe462c8c
Behruz <behruz@macshops-C337T571V9.local>
2025-08-11 05:03:54 +0500

    fixed

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,22 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}))
   await app.listen(process.env.SERVER_PORT ?? 3000);
 
-  // const serverUrl = process.env.API_BASE_URL // ← здесь получаешь URL
-  // console.log(`Server running on: ${serverUrl}`);
+  const serverUrl = process.env.API_BASE_URL // ← здесь получаешь URL
+  console.log(`Server running on: ${serverUrl}`);
 
-  // const trelloKey = process.env.TRELLO_KEY;
-  // const trelloToken = process.env.TRELLO_TOKEN;
-  // const trelloModelId = process.env.TRELLO_BOARD_ID; // или idList
+  const trelloKey = process.env.TRELLO_KEY;
+  const trelloToken = process.env.TRELLO_TOKEN;
+  const trelloModelId = process.env.TRELLO_BOARD_ID; // или idList
 
-  // try {
-  //   await axios.post(`https://api.trello.com/1/webhooks?key=${trelloKey}&token=${trelloToken}`, {
-  //     description: 'My Trello Webhook',
-  //     callbackURL: `${serverUrl}/webhook/trello`, // наш обработчик в контроллере
-  //     idModel: trelloModelId
-  //   });
-  //   console.log('Trello webhook created successfully');
-  // } catch (error) {
-  //   console.error('Error creating Trello webhook:', error.response?.data || error.message);
-  // }
+  try {
+    await axios.post(`https://api.trello.com/1/webhooks?key=${trelloKey}&token=${trelloToken}`, {
+      description: 'My Trello Webhook',
+      callbackURL: `${serverUrl}/webhook/trello`, // наш обработчик в контроллере
+      idModel: trelloModelId
+    });
+    console.log('Trello webhook created successfully');
+  } catch (error) {
+    console.error('Error creating Trello webhook:', error.response?.data || error.message);
+  }
 }
 bootstrap();
