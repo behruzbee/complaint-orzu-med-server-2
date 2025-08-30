50a857dcf25ebb175a15ec826ee1c1cca7b73b51
Behruzbee <behruz.baxtiyorov.dev@gmail.com>
2025-08-30 15:37:43 +0500

    main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://orzu-medical.vercel.app"
+    origin: "*"
   })
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}))
   await app.listen(process.env.SERVER_PORT ?? 3000);
